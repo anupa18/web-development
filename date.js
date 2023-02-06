7d6f88103560d64f28e0e7feff39b3f7102786d0
anupa18 <kunduanupam1@gmail.com>
2023-02-06 10:53:30 +0530

    deleted  many file

--- date.js
+++
@@ -1,6 +0,0 @@
-const date=new Date();
-var day=date.getDay();
-var d=date.getDate();
-var m=date.getMonth();
-var y=date.getFullYear();
-console.log(y);
--- new.js
+++
@@ -1,7 +0,0 @@
-let a=3;
-let b=4;
-let temp=a;
-a=b;
-b=temp;
-console.log(a);
-console.log(b);
