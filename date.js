2df7075085e405ca4ae91e56beb3725a8eaf500f
anupa18 <kunduanupam1@gmail.com>
2023-02-06 09:54:26 +0530

    dom1 commit here

---
+++ date.js
@@ -0,0 +1,6 @@
+const date=new Date();
+var day=date.getDay();
+var d=date.getDate();
+var m=date.getMonth();
+var y=date.getFullYear();
+console.log(y);
---
+++ dom.js
@@ -0,0 +1,12 @@
+// console.dir(document);
+// console.log(document.all[10]);
+var texttitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+var cls=document.getElementById('cl');
+console.log(texttitle.textContent)
+// texttitle.textContent='Hello';
+// texttitle.innerHTML='<h3>Hi</h3>';
+//texttitle.style.borderBottom='solid 2px #000';
+header.style.borderBottom='solid 2px #000';
+cls.style.color='green';
+cls.style.fontWeight='bold';
