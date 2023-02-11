fbf9482f2ea23f075c14ab41607ec022f88da8b0
anupa18 <kunduanupam1@gmail.com>
2023-02-11 11:31:47 +0530

    adding new dom.js

--- dom.js
+++ dom.js
@@ -3,10 +3,15 @@
 var texttitle=document.getElementById('header-title');
 var header=document.getElementById('main-header');
 var cls=document.getElementById('cl');
-console.log(texttitle.textContent)
+var items=document.getElementsByClassName('list-group-item');
+// console.log(texttitle.textContent)
 // texttitle.textContent='Hello';
 // texttitle.innerHTML='<h3>Hi</h3>';
 //texttitle.style.borderBottom='solid 2px #000';
 header.style.borderBottom='solid 2px #000';
 cls.style.color='green';
 cls.style.fontWeight='bold';
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
