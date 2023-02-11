ca02bee2fc1f4cb9d5690eff079c30b90aaacdf2
anupa18 <kunduanupam1@gmail.com>
2023-02-11 12:42:10 +0530

    add getElementsByTagName method in code

--- dom.js
+++ dom.js
@@ -1,17 +1,22 @@
 // console.dir(document);
 // console.log(document.all[10]);
-var texttitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
-var cls=document.getElementById('cl');
+// var texttitle=document.getElementById('header-title');
+// var header=document.getElementById('main-header');
+// var cls=document.getElementById('cl');
 var items=document.getElementsByClassName('list-group-item');
+var li=document.getElementsByTagName('li');
 // console.log(texttitle.textContent)
 // texttitle.textContent='Hello';
 // texttitle.innerHTML='<h3>Hi</h3>';
 //texttitle.style.borderBottom='solid 2px #000';
-header.style.borderBottom='solid 2px #000';
-cls.style.color='green';
-cls.style.fontWeight='bold';
-items[2].style.backgroundColor='green';
+// header.style.borderBottom='solid 2px #000';
+// cls.style.color='green';
+// cls.style.fontWeight='bold';
+// li[2].style.backgroundColor='green';
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+// }
+//li[2].style.backgroundColor='green';
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
