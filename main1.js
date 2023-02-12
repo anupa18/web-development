b6f0ed75b25830d4c1db12d4fb654769d2251fa1
anupa18 <kunduanupam1@gmail.com>
2023-02-12 21:09:50 +0530

    add filter function

--- main1.js
+++ main1.js
@@ -1,5 +1,6 @@
 var form=document.getElementById('addForm');
 var itemlist=document.getElementById('items');
+var filter=document.getElementById('filter');
 
 //form submmit event
 form.addEventListener('submit', addItem);
@@ -7,20 +8,23 @@ form.addEventListener('submit', addItem);
 //delete evenet
 itemlist.addEventListener('click', removeItem);
 
+//filter event
+filter.addEventListener('keyup',filteritem);
+
 //Add item function
 function addItem(e){
     e.preventDefault();
 
     //get input value
     var newitem=document.getElementById('item').value;
-
+    var newitem1=document.getElementById('description').value;
     //create new li ee
     var li= document.createElement('li');
     //add class
     li.className='list-group-item';
     //add text node
     li.appendChild(document.createTextNode(newitem));
-
+    li.appendChild(document.createTextNode(newitem1));
      //add delete button
      var delbtn=document.createElement('button');
      //add class to delete button
@@ -55,4 +59,26 @@ function removeItem(e){
             itemlist.removeChild(li);
         }
     }
+}
+
+//filter function
+function filteritem(e){
+    //convert text to lower case 
+    var text=e.target.value.toLowerCase();
+    console.log(text);
+    //get list
+    var items=itemlist.querySelectorAll('li');
+    console.log(items);
+    //convert to an arry
+    Array.from(items).forEach(function(item){
+        var itemName=item.firstChild.textContent;
+        var itemDescription=item.childNodes[1].textContent;
+        console.log(itemDescription);
+        if(itemName.toLowerCase().indexOf(text)!=-1 || itemDescription.toLowerCase().indexOf(text)!=-1){
+            item.style.display='block';
+        }else{
+            item.style.display='none';
+        }
+    })
+    
 }
